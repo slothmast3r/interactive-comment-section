aaf1601e361355f836a52fbe565f7bcd0a9b89ee
slothmast3r <oskar.straszynski@gmail.com>
2022-01-12 16:17:46 +0100

    import fonts

--- src/App.vue
+++ src/App.vue
@@ -21,12 +21,14 @@ export default {
 
 <style lang="scss">
 @import "./scss/_variables.scss";
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Rubik', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 16px;
 }
 </style>
