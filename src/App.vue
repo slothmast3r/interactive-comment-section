23ad9b498f8a2225865cd8d191be8501fc2cef7f
slothmast3r <oskar.straszynski@gmail.com>
2022-01-12 15:43:22 +0100

    xd

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-    Coded by <a href="https://github.com/slothmast3r">slothmast3r</a>.
+  <div>
+    <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="https://github.com/slothmast3r">slothmast3r</a>.
+    </div>
   </div>
 </template>
 
