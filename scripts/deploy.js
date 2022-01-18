19672bcdc8f6cc089c013bed2e305bb7ae4fa484
slothmast3r <oskar.straszynski@gmail.com>
2022-01-18 22:30:38 +0100

    xd

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,7 @@ import fs from "fs";
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         // eslint-disable-next-line no-console
         console.log("Building started...");
-        await execa("npm", ["run", "build"]);
+        // await execa("npm", ["run", "build"]);
         // Understand if it's dist or build folder
         const folderName = fs.existsSync("dist") ? "dist" : "build";
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
